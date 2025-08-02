77e569d3f613e8f7948b49e645f5c7bab300a20d
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-08-02 18:48:03 +0000

    updated

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-    except IndexError:
-        x = i
-    finally:
-        print()
-        return x
+"""An emppty class Square
+"""
+
+
+class Square:
+    """That defines a square
+    """
+    pass
