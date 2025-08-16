2a7ff0920000d48bbbafb2ccc9583f4c152d77ce
RaghadAlbeladi <Raghadz_5@hotmail.com>
2025-08-16 10:56:51 +0000

    a

---
+++ python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module returns dict"""
+
+
+class Student:
+    """Student Class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        return self.__dict__.copy()
